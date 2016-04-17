d6b5cd921591d17ba6277180785495f79467deaa
Biernat <biernatm@USBBCOBIERNATM.local>
2016-04-17 17:01:38 -0400

    Developed the main simulation.

--- run_sim.py
+++ run_sim.py
@@ -15,6 +15,7 @@ class StatsSums:
         self.total_delivery_distance = 0.0
         self.total_houses = 0
         self.total_departs = 0
+        self.inventory = 0
 
 
 class Statistics:
@@ -23,11 +24,14 @@ class Statistics:
         self.avg_delivery_distance = 0.0
         self.avg_travel_time = 0.0
         self.avg_delivery_time = 0.0
+        self.avg_houses_per_trip = 0.0
+        self.total_houses = 0
 
 class TimeStructure:
     def __init__(self):
         self.departure = -1       # time delivery driver departs
         self.done = -1       # time the next pizza is done
+        self.done_times = -1
         self.demand = -1       # time of next call-in for delivery
         self.arrival = -1       # time when the delivery driver arrives at the pizza shop
         self.current = -1       # current time
@@ -45,7 +49,7 @@ def run_sim(gap, low, high, mode, maxTime):
     infinity = math.inf
 
     t.demand = getDemand(low, high, mode)
-    t.done = infinity
+    t.done_times = []
     t.departure = infinity
     t.arrival = infinity
     t.current = 0
@@ -53,12 +57,22 @@ def run_sim(gap, low, high, mode, maxTime):
     delivery_coords = []
 
     while t.current < maxTime:
+        if not t.done_times:
+            t.done = infinity
+        else:
+            t.done = t.done_times[0]
         next_event = min(t.demand, t.arrival, t.done, t.departure)
         t.current = next_event
+        '''print("demand: " + str(t.demand))
+        print("arrival: " + str(t.arrival))
+        print("done: " + str(t.done))
+        print("depart: " + str(t.departure))
+        print("-----------------------------------")'''
 
         if next_event == t.demand:
+            #print("demand")
             if t.current < maxTime:
-                t.done = t.current + 15
+                t.done_times.append(t.current + 15)
                 delivery_coords.append(ng.get_order())
                 t.demand = t.current + getDemand(low, high, mode)
                 stat_sums.total_houses += 1
@@ -66,30 +80,34 @@ def run_sim(gap, low, high, mode, maxTime):
                 t.demand = infinity
 
         if next_event == t.done:
+            #print("done")
+            t.done_times.pop(0)
+            stat_sums.inventory += 1
+
             if t.departure == infinity:
                 if t.arrival == infinity:
                     t.departure = t.current + gap
                 else:
                     if t.current + gap < t.arrival:
-                        t.departure = t.arrival
+                        t.departure = t.arrival + 1
                     else:
                         t.current + gap
 
         if next_event == t.departure:
+            #print("depart")
             deliver_route = travel_path.get_minPath(delivery_coords)
-            for item in deliver_route:
-                total_distance = 0
-                x1 = 0
-                y1 = 0
-                for house in range(0, len(item)):
-                    x2 = item[house][0]
-                    y2 = item[house][1]
+            total_distance = 0
+            x1 = 0
+            y1 = 0
+            for house in deliver_route:
+                x2 = house[0]
+                y2 = house[1]
 
-                    d = math.sqrt(((x2-x1)**2) + ((y2-y1)**2))
-                    total_distance += d
+                d = math.sqrt(((x2-x1)**2) + ((y2-y1)**2))
+                total_distance += d
 
-                    x1 = x2
-                    y1 = y2
+                x1 = x2
+                y1 = y2
             stat_sums.total_delivery_distance += total_distance
 
             d = math.sqrt(((x2-0)**2) + ((y2-0)**2))
@@ -97,10 +115,14 @@ def run_sim(gap, low, high, mode, maxTime):
 
             travel_time = stat_sums.total_travel_distance * 1.72
             t.arrival = t.current + travel_time
+            #print("arrive: " + str(t.arrival))
             t.departure = infinity
             stat_sums.total_departs += 1
+            delivery_coords = []
+            stat_sums.inventory = 0
 
         if next_event == t.arrival:
+            #print("arrival")
             t.arrival = infinity
 
     stats = Statistics()
@@ -109,6 +131,8 @@ def run_sim(gap, low, high, mode, maxTime):
     stats.avg_delivery_distance = stat_sums.total_delivery_distance / stat_sums.total_houses
     stats.avg_travel_time = stats.avg_travel_distance * 1.72
     stats.avg_delivery_time = stats.avg_delivery_distance * 1.72
+    stats.avg_houses_per_trip = stat_sums.total_houses / stat_sums.total_departs
+    stats.total_houses = stat_sums.total_houses
 
     return stats
 
@@ -116,14 +140,16 @@ def run_sim(gap, low, high, mode, maxTime):
 
 def main():
 
-    stats = run_sim(5, 1, 10, 5, 720)
+    stats = run_sim(5, 15, 25, 20, 720)
+    print(stats.avg_travel_distance)
+    #print(stats.avg_delivery_distance)
+    print(stats.avg_travel_time)
+    #print(stats.avg_delivery_time)
+    print(stats.avg_houses_per_trip)
+    print(stats.total_houses)
+    print("-------------------")
 
-    print("Average travel distance {1:6.2f}".format(stats.avg_travel_distance))
-    print("Average delivery distance {0:4.2f}".format(stats.avg_delivery_distance))
-    print("Average travel time = ({0},{1})\n".format(stats.avg_travel_time))
-    print("Average delivery distance = {0:6.2f}".format(stats.avg_delivery_time))
 
-    print("here")
 
-if __name__ == 'main':
+if __name__ == '__main__':
     main()
